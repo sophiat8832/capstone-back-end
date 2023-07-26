7d672792e5b0312780f5f62cdcbf0d2eca7e4a08
Lily <aulily09@gmail.com>
2023-07-26 13:52:42 -0700

    Implement POST endpoint for restaurants collection

--- routes/record.mjs
+++ routes/record.mjs
@@ -1,6 +1,10 @@
 import express from "express";
 import db from "../db/conn.mjs";
 import { ObjectId } from "mongodb";
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+
 
 const router = express.Router();
 
@@ -18,7 +22,7 @@ router.get("/", async (req, res) => {
 // This section will help you get a single record by id
 router.get("/:id", async (req, res) => {
     let collection = await db.collection("users");
-    let query = {_id: new ObjectId(req.params.id)};
+    let query = { _id: new ObjectId(req.params.id) };
     let result = await collection.findOne(query);
 
     if (!result) res.send("Not found").status(404);
@@ -43,11 +47,11 @@ router.post("/", async (req, res) => {
 // This section will help you update a record by id.
 router.patch("/:id", async (req, res) => {
     const query = { _id: new ObjectId(req.params.id) };
-    const updates =  {
+    const updates = {
         $set: {
-        name: req.body.name,
-        position: req.body.position,
-        level: req.body.level
+            name: req.body.name,
+            position: req.body.position,
+            level: req.body.level
         }
     };
 
@@ -62,6 +66,81 @@ router.delete("/:id", async (req, res) => {
     const query = { _id: new ObjectId(req.params.id) };
 
     const collection = db.collection("records");
+    let result = await collection.deleteOne(query);
+
+    res.send(result).status(200);
+});
+
+
+// *********** RESTAURANT COLLECTION ******************
+
+// This section will help you get a list of all the records.
+router.get("/", async (req, res) => {
+    console.log("Testing")
+    let collection = await db.collection("restaurants");
+    let results = await collection.find({}).toArray();
+
+    console.log("THIS IS COLLECTION", collection)
+    console.log("THIS IS RESULTS", results)
+    res.send(results).status(200);
+});
+
+// This section will help you get a single record by id
+router.get("/:id", async (req, res) => {
+    let collection = await db.collection("restaurants");
+    let query = { _id: new ObjectId(req.params.id) };
+    let result = await collection.findOne(query);
+
+    if (!result) res.send("Not found").status(404);
+    else res.send(result).status(200);
+});
+
+// This section will help you create a new record.
+router.post("/new-restaurant", async (req, res) => {
+
+    const YELP_API = process.env.YELP_API_KEY;
+    const sdk = require('api')('@yelp-developers/v1.0#29blk6qj5xa');
+    sdk.auth(`Bearer ${YELP_API}`);
+
+    let searchRestaurant = {
+        location: req.body.location,
+        term: req.body.term,
+    };
+
+    sdk.v3_business_search(searchRestaurant)
+        .then(({ data }) => {
+            let collection = db.collection("restaurants");
+            let result = collection.insertOne(data.businesses[0]);
+            res.send(data.businesses[0]).status(204);
+        })
+        .catch(err => {
+            console.error(err);
+            res.send("Error occurred while fetching restaurant data").status(500);
+        });
+});
+
+// This section will help you update a record by id.
+router.patch("/:id", async (req, res) => {
+    const query = { _id: new ObjectId(req.params.id) };
+    const updates = {
+        $set: {
+            name: req.body.name,
+            position: req.body.position,
+            level: req.body.level
+        }
+    };
+
+    let collection = await db.collection("restaurants");
+    let result = await collection.updateOne(query, updates);
+
+    res.send(result).status(200);
+});
+
+// This section will help you delete a record
+router.delete("/:id", async (req, res) => {
+    const query = { _id: new ObjectId(req.params.id) };
+
+    const collection = db.collection("restaurants");
     let result = await collection.deleteOne(query);
 
     res.send(result).status(200);
