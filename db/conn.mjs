d1c70bae15df0672fad4b3053ff739f5955dfc31
Lily <aulily09@gmail.com>
2023-07-27 14:31:21 -0700

    Add message to error

--- db/conn.mjs
+++ db/conn.mjs
@@ -7,8 +7,8 @@ const client = new MongoClient(connectionString);
 let conn;
 try {
   conn = await client.connect();
-} catch(e) {
-  console.error(e);
+} catch (e) {
+  console.error("THIS IS THE ERROR!!!!", e);
 }
 
 let db = conn.db("capstonedb");
