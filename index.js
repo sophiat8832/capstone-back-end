266e5d84fca42847a891ed0791c3b2ccc98e2602
Sophia Tran <sophiat8832@gmail.com>
2023-09-14 09:31:37 -0700

    export endpoints

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use("/record", records);
 
 // start the Express server
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port: ${PORT}`);
+// });
+module.exports = app;
